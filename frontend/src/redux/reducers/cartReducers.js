4e7c107762c045c68e546c4132393bbc8f9de25f
syngyang <syngyang@gmail.com>
2021-09-29 20:24:47 +0900

    cartReducer 추가

---
+++ frontend/src/redux/reducers/cartReducers.js
@@ -0,0 +1,29 @@
+import * as actionType from '../constants/cartConstants'
+
+export const cartReducer = (state = {cartItem: [] }, action)=> {
+    switch(action.type) {
+        case actionTypes.ADD_TO_CART:
+            const item = action.payload;
+            const existItem = state.cartItems.find((p)=> p.product === item.product)
+
+            if (existItem) {
+                return {
+                    ...state,
+                    cartItems: state.cartItem.map((x)=> x.product === existItem.product ? item:x )
+                }
+            } else {
+                return {
+                    ...state,
+                    cartItems: [...state.cartItems, item],
+                }
+            }
+        case actionTypes,RENOVE_FROM_CART:
+            return {
+                ...state,
+                cartItems: state.cartItems.filter((x)=> x.product !== action.payload)
+            }
+        
+        default:
+            return state;
+    }
+}
